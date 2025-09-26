b1f84b713653a047c1e37f05f838f65278048237
vasanth2896 <vnayak2896@gmail.com>
2025-09-26 19:14:40 +0530

    Vasanth | Completed Trending skills

---
+++ src/components/chip/Chip.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <span class="chip__container">
+    <slot></slot>
+  </span>
+</template>
+
+<style scoped lang="scss">
+@use '../../styles/variables.scss' as *;
+@use '../../styles/mixins.scss' as *;
+
+.chip__container{
+   
+    background-color: #E4EDF6;
+    padding: 6px 20px;
+    border-radius: 20px;
+    font-weight: 500;
+    font-size: 17px;
+}
+</style>
