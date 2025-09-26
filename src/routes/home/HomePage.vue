481a8e66ad4130e5023cb4722609a4314556af4b
vasanth2896 <vnayak2896@gmail.com>
2025-09-26 19:14:40 +0530

    Vasanth | Completed Trending skills

--- src/routes/home/HomePage.vue
+++ src/routes/home/HomePage.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import Chip from '@/components/chip/Chip.vue'
 import ImageCard from '@/components/imageCard/ImageCard.vue'
-import { services } from '@/data/services';
+import { services } from '@/data/services'
+import { industry } from '@/data/trendingSkills'
 </script>
 
 <template>
@@ -71,15 +73,42 @@ import { services } from '@/data/services';
           Suspendisse in tellus eget felis congue convallis ac sit amet velit.
         </p>
         <div class="services__lists">
-          <ImageCard class="image_card"  v-for="service in services" :image="service.image">
-            <p style="text-align: center;">{{ service.title }}</p>
-            <p><RouterLink :to="`services/${service.slug}`">click here to learn more</RouterLink></p>
+          <ImageCard class="image_card" v-for="service in services" :image="service.image">
+            <p style="text-align: center">{{ service.title }}</p>
+            <p>
+              <RouterLink :to="`services/${service.slug}`">click here to learn more</RouterLink>
+            </p>
           </ImageCard>
         </div>
       </div>
     </section>
 
-    
+    <section class="trending__skills">
+      <h1 class="trending__skills__title title__embelish">Trending skills</h1>
+      <div class="trending__skills__content">
+        <p class="content__text">
+          The most in-demand professionals today are those who can blend core enterprise skills with
+          forward-looking technologies. This means expertise in Cloud Computing across major
+          platforms (AWS, Azure, GCP) coupled with strong DevOps/SRE and Cloud Security practices is
+          foundational. Within the enterprise application sphere, the primary focus is on SAP
+          S/4HANA migration and leveraging its in-built AI/Data Analytics capabilities, while in the
+          Customer Relationship Management (CRM) domain, demand is surging for Salesforce experts
+          skilled in Generative AI (Einstein GPT), Flow Automation (low-code), and seamless
+          API/MuleSoft integration. Ultimately, the trending skills across all sectors of the "New
+          Tech" landscape—from AI and Data Science to Automation—revolve around utilizing these core
+          cloud and platform environments to drive intelligent, secure, and automated business
+          processes.
+        </p>
+        <div class="trending__skillsets">
+          <div class="skillList" v-for="ind in industry">
+            <h3>{{ ind.title }}</h3>
+            <div v-for="skill in ind.skillSets" :key="skill.id" class="skills">
+              <Chip>{{ skill.name }}</Chip>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <section class="offer">
       <h1 class="offer__title title__embelish">What We Offer</h1>
@@ -110,4 +139,5 @@ import { services } from '@/data/services';
 @use './styles/about.scss' as *;
 @use './styles/offer.scss' as *;
 @use './styles/services.scss' as *;
+@use './styles/trendingSkills.scss' as *;
 </style>
